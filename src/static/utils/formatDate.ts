55800d9bb43234563b9f221665fbf597b2133cc6
Dave Bitter <dbitter@mirabeau.nl>
2020-04-23 15:28:18 +0200

    chore(format-date): Added test

--- src/static/utils/formatDate.ts
+++ src/static/utils/formatDate.ts
@@ -1,5 +1,5 @@
 export default (date: string, format: any, locale: string = 'en') => {
-    if (!date) { return; }
+    if (!date) { return null; }
 
     date = date.toString();
     return (date.includes(':') || date.includes('-') || date.includes('/')) ? new Intl.DateTimeFormat(locale, format).format(new Date(date)) : date;
