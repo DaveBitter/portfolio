9299ff9cf9f8ab3c78f66235d34dbd222aa0c9da
Dave Bitter <dbitter@mirabeau.nl>
2020-04-23 17:11:59 +0200

    chore(articleTeaser): Added test

---
+++ src/components/Article/ArticleTeaser/ArticleTeaser.test.tsx
@@ -0,0 +1,33 @@
+// Libs
+import React from 'react';
+import ArticleTeaser from './ArticleTeaser';
+import { mount } from 'enzyme';
+import { getArticles } from '../../../static/utils/getContent';
+
+// Test constants
+const mockArticles = getArticles().items
+
+// Component test setup
+const setup = (props: any) => {
+    const _props = {
+        ...props
+    };
+
+    const wrapper = mount(<ArticleTeaser {..._props} />);
+
+    return {
+        _props,
+        wrapper
+    };
+};
+
+// Test scenarios
+describe('<ArticleTeaser />', () => {
+    it('should mount', () => {
+        const { wrapper } = setup({
+            ...mockArticles[0]
+        });
+
+        expect(wrapper.childAt(0).exists());
+    });
+});
