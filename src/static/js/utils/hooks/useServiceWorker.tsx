ad8d454bd0fdea64a1b2bd5918057901baf82637
Dave Bitter <dbitter@mirabeau.nl>
2020-08-11 13:35:19 +0200

    feat(service-worker): Added prompt

--- src/static/js/utils/hooks/useServiceWorker.tsx
+++ src/static/js/utils/hooks/useServiceWorker.tsx
@@ -1,12 +1,27 @@
+// Libs
 import { useEffect } from 'react';
 
+// Utils
 import registerServiceWorker from '../registerServiceWorker';
 
+// Helpers
+const onBeforeInstallPrompt = (e: any) => {
+    setTimeout(() => {
+        e.prompt();
+    }, 1000);
+};
+
+// Component
 const useServiceWorker = (): null => {
-    useEffect((): void => {
+    useEffect(() => {
         if (process.env.NODE_ENV !== 'production') { return; }
 
+        window.addEventListener('beforeinstallprompt', onBeforeInstallPrompt);
+
+
         registerServiceWorker();
+
+        return () => window.removeEventListener('beforeinstallprompt', onBeforeInstallPrompt);
     }, []);
 
     return null;
