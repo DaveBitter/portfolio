4d96a49f3f11975c938a24b982a7c663e28642ff
Dave Bitter <dbitter@mirabeau.nl>
2020-09-01 16:53:39 +0200

    chore(a2h): Removed a2h

--- src/static/js/utils/hooks/useServiceWorker.tsx
+++ src/static/js/utils/hooks/useServiceWorker.tsx
@@ -5,23 +5,13 @@ import { useEffect } from 'react';
 import registerServiceWorker from '../registerServiceWorker';
 
 // Helpers
-const onBeforeInstallPrompt = (e: any) => {
-    setTimeout(() => {
-        e.prompt();
-    }, 1000);
-};
 
 // Component
 const useServiceWorker = (): null => {
     useEffect(() => {
         if (process.env.NODE_ENV !== 'production') { return; }
 
-        window.addEventListener('beforeinstallprompt', onBeforeInstallPrompt);
-
-
         registerServiceWorker();
-
-        return () => window.removeEventListener('beforeinstallprompt', onBeforeInstallPrompt);
     }, []);
 
     return null;
