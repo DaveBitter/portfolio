d1829a42201dd245ad6b478f73d064aac467b6cc
Dave Bitter <dbitter@mirabeau.nl>
2021-05-07 15:28:57 +0200

    wip

--- src/static/js/utils/api/query.ts
+++ src/static/js/utils/api/query.ts
@@ -1,22 +1,39 @@
-import { getArticles, getCopy, getDictionary, getEducation, getHeadings, getQuickBits, getTags, getWorkExperience } from "../getContent";
+// import { getArticles, getCopy, getDictionary, getEducation, getHeadings, getQuickBits, getTags, getWorkExperience } from "../getContent";
+
+// const fetchFromLocalMarkdown = (path: string) => {
+//     switch (path) {
+//         case '/content/articles':
+//             return { articles: getArticles() };
+//         case '/content/quick-bits':
+//             return { quickBits: getQuickBits() };
+//         case '/content/tags':
+//             return { tags: getTags() };
+//         case '/content/ui':
+//             return { dictionary: getDictionary(), copy: getCopy(), headings: getHeadings() };
+//         case '/content/work-and-education':
+//             return { workExperience: getWorkExperience(), education: getEducation() };
+//         default:
+//             return {};
+//     }
+// };
 
 const fetchFromLocalMarkdown = (path: string) => {
     switch (path) {
         case '/content/articles':
-            return { articles: getArticles() };
+            return { articles: [] };
         case '/content/quick-bits':
-            return { quickBits: getQuickBits() };
+            return { quickBits: [] };
         case '/content/tags':
-            return { tags: getTags() };
+            return { tags: {} };
         case '/content/ui':
-            return { dictionary: getDictionary(), copy: getCopy(), headings: getHeadings() };
+            return { dictionary: {}, copy: {}, headings: {} };
         case '/content/work-and-education':
-            return { workExperience: getWorkExperience(), education: getEducation() };
+            return { workExperience: [], education: [] };
         default:
             return {};
     }
 };
 
-let baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://davebitter.com`;
+let baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://davebsitter.com`;
 
 export default async (path: string) => await fetch(`${baseUrl}/api${path}`).then((res) => res.json()).catch(() => fetchFromLocalMarkdown(path));
