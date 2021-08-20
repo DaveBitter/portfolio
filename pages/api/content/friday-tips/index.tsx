501f76e6cae4cd95e5c46d65d33937909549f903
Dave Bitter <dbitter@anwb.nl>
2021-08-20 09:15:00 +0200

    fix(friday-tips): Fix order

--- pages/api/content/friday-tips/index.tsx
+++ pages/api/content/friday-tips/index.tsx
@@ -12,7 +12,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const fridayTips = items
         .filter(({ snippet }: any) => snippet.title !== 'Private video')
-        .map(convertYoutubeDataToFridayTip).reverse();
+        .map(convertYoutubeDataToFridayTip);
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
