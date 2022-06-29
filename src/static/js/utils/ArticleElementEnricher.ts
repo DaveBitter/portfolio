6ed8f96a12bbf3007acd78f0af717bb74bf42766
Dave Bitter <dbitter@anwb.nl>
2022-06-29 09:47:50 +0200

    feat(article): Add face cropper article

--- src/static/js/utils/ArticleElementEnricher.ts
+++ src/static/js/utils/ArticleElementEnricher.ts
@@ -2,79 +2,89 @@
 import isExternalURL from "./isExternalUrl";
 
 class ArticleElementEnricher {
-    constructor(element: any, options: any) {
-        if (!element) { return; }
-        this._element = element;
-        this._options = options
-
-        this._init();
+  constructor(element: any, options: any) {
+    if (!element) {
+      return;
     }
+    this._element = element;
+    this._options = options;
 
-    _element: any = null
-    _options: any = null
-    _links: any = []
-    _images: any = []
-    _videos: any = []
+    this._init();
+  }
 
-    _enrichLinks() {
-        this._links.forEach((link: any) => {
-            if (isExternalURL(link.href)) {
-                link.setAttribute('target', '__blank');
-                link.setAttribute('rel', 'noopener noreferrer');
-            }
-        });
-    }
+  _element: any = null;
+  _options: any = null;
+  _links: any = [];
+  _images: any = [];
+  _videos: any = [];
 
-    _enrichImages() {
-        this._images.forEach((img: any) => {
-            const parentNode = img.parentNode;
-            const imgClone = img.cloneNode();
-            const sibling = img.nextElementSibling;
-            const figure = document.createElement('figure');
-            imgClone.setAttribute('loading', 'lazy');
-            figure.dataset.figure = '';
+  _enrichLinks() {
+    this._links.forEach((link: any) => {
+      if (isExternalURL(link.href)) {
+        link.setAttribute("target", "__blank");
+        link.setAttribute("rel", "noopener noreferrer");
+      }
+    });
+  }
 
-            img.parentNode.removeChild(img);
-            figure.appendChild(imgClone);
+  _enrichImages() {
+    this._images.forEach((img: any) => {
+      const parentNode = img.parentNode;
+      const imgClone = img.cloneNode();
+      const sibling = img.nextElementSibling;
+      const figure = document.createElement("figure");
+      imgClone.setAttribute("loading", "lazy");
+      figure.dataset.figure = "";
 
-            if (sibling && sibling.tagName != 'p') {
-                const caption = document.createElement('figcaption');
-                caption.textContent = sibling.textContent;
+      parentNode?.removeChild(img);
+      figure.appendChild(imgClone);
 
-                figure.appendChild(caption);
-                parentNode.removeChild(sibling);
-            }
+      if (sibling && sibling.tagName != "p") {
+        const caption = document.createElement("figcaption");
+        caption.textContent = sibling.textContent;
 
-            parentNode.appendChild(figure);
-        });
-    }
+        figure.appendChild(caption);
+        parentNode?.removeChild(sibling);
+      }
 
-    _enrichVideos() {
-        this._videos.forEach((video: any) => {
-            const parentNode = video.parentNode;
-            const videoClone = video.cloneNode();
-            const wrapper = document.createElement('div');
-            wrapper.classList.add('article__video-wrapper')
-            videoClone.classList.add('article__video')
+      parentNode?.appendChild(figure);
+    });
+  }
 
-            video.parentNode.removeChild(video);
-            wrapper.appendChild(videoClone);
-            parentNode.appendChild(wrapper);
-        });
-    }
+  _enrichVideos() {
+    this._videos.forEach((video: any) => {
+      const parentNode = video.parentNode;
+      const videoClone = video.cloneNode();
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("article__video-wrapper");
+      videoClone.classList.add("article__video");
 
-    _cacheSelectors() {
-        this._images = [...this._element.querySelectorAll('img')].filter(img => !img.classList.length);
-        this._links = [...this._element.querySelectorAll('a')].filter(img => !img.classList.length);
-        this._videos = [...this._element.querySelectorAll('iframe[src^="https://www.youtube.com/embed"]')].filter(video => !video.classList.length);
-    }
+      video.parentNode.removeChild(video);
+      wrapper.appendChild(videoClone);
+      parentNode.appendChild(wrapper);
+    });
+  }
 
-    _init() {
-        this._cacheSelectors();
-        this._enrichImages();
-        this._enrichVideos();
-        this._enrichLinks();
-    }
+  _cacheSelectors() {
+    this._images = [...this._element.querySelectorAll("img")].filter(
+      (img) => !img.classList.length
+    );
+    this._links = [...this._element.querySelectorAll("a")].filter(
+      (img) => !img.classList.length
+    );
+    this._videos = [
+      ...this._element.querySelectorAll(
+        'iframe[src^="https://www.youtube.com/embed"]'
+      ),
+    ].filter((video) => !video.classList.length);
+  }
+
+  _init() {
+    this._cacheSelectors();
+    this._enrichImages();
+    this._enrichVideos();
+    this._enrichLinks();
+  }
 }
 
 export default ArticleElementEnricher;
